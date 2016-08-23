b1c784c51c95615a92d0f82141fd26bb456a4e09
Stephan Kotze <Stephanus.Kotze@gmail.com>
2016-08-23 07:57:53 +0100

    improvements to create_release_image.sh

--- scripts/build_release_image/create_release_image.sh
+++ scripts/build_release_image/create_release_image.sh
@@ -1,16 +1,20 @@
+#$1 = directory in which the .tgz artefact is to be found
+#$2 the label we want to attach to the created image
 #sample usage:
-#./create_in_jenkins.sh ~/ci_volumes/jenkins/var/jenkins_home/workspace/compile_and_test/target/universal/ play-java-intro-1.0-SNAPSHOT play-java-intro:1.0-SNAPSHOT
+#./create_in_jenkins.sh ~/ci_volumes/jenkins/var/jenkins_home/workspace/compile_and_test/target/universal play-java-intro:1.0-SNAPSHOT
+
+
+TGZ_ARTEFACT=$(ls $1/*.tgz)
+RELEASE_ARTEFACT_NAME=$(echo $TGZ_ARTEFACT | sed s/\.tgz\//)
+
 set -x
 sudo docker pull $DOCKERHOST_HOSTNAME:$REGISTRY_PORT/java8_base:latest
 sudo docker tag $DOCKERHOST_HOSTNAME:$REGISTRY_PORT/java8_base:latest java8_base:latest
 cp $1/$2.tgz .
 
-sudo docker build --build-arg archive=$2 -t $3 .
+sudo docker build --build-arg archive=$RELEASE_ARTEFACT_NAME -t $2 .
 
 
-#git clone https://github.com/skotze/testrepo
-#cd testrepo/devstackimage/compose/ci_stack/project_images/release_image
-#/scripts/create_in_jenkins.sh ../target/universal/ play-java-intro-1.0-SNAPSHOT play-java-intro:1.0-SNAPSHOT
 
 
 
