1c28f522f81b37b97bab72021daae586c1d67206
Stephan Kotze <Stephanus.Kotze@gmail.com>
2016-08-24 17:00:42 +0100

    fixing release scripts

--- scripts/build_release_image/create_release_image.sh
+++ scripts/build_release_image/create_release_image.sh
@@ -4,14 +4,12 @@
 #./create_in_jenkins.sh ~/ci_volumes/jenkins/var/jenkins_home/workspace/compile_and_test/target/universal play-java-intro:1.0-SNAPSHOT
 #############################
 #Sample jenkins call:
-# cd $WORKSPACE/build_utils/scripts/build_release_image
-# chmod +x $WORKSPACE/build_utils/scripts/docker_utils/docker_push.sh
-# chmod +x $WORKSPACE/build_utils/scripts/docker_utils/docker_tag.sh
 # PROJECT_NAME="play-java-intro"
 # DOCKER_IMAGE_TAG="play-java-intro:"$BUILD_TAG
 # ARTEFACT_DIR=$WORKSPACE/target/universal
 # ARTEFACT_NAME=$(ls $ARTEFACT_DIR/*.tgz | xargs -n1 basename)
 # ARTEFACT_VERSION=$(echo $ARTEFACT_NAME | sed s/\.tgz\// | sed s/$PROJECT_NAME// | cut -c 2-)
+# cd $WORKSPACE/build_utils/scripts/build_release_image/
 # ./create_release_image.sh $WORKSPACE/target/universal $DOCKER_IMAGE_TAG
 # #appends repo details for push
 # $WORKSPACE/build_utils/scripts/docker_utils/docker_tag.sh $DOCKER_IMAGE_TAG $DOCKER_IMAGE_TAG
@@ -33,7 +31,7 @@ sudo docker pull $DOCKERHOST_HOSTNAME:$REGISTRY_PORT/java8_base:latest
 sudo docker tag $DOCKERHOST_HOSTNAME:$REGISTRY_PORT/java8_base:latest java8_base:latest
 cp $1/$TGZ_ARTEFACT .
 
-sudo docker build -f $WORKSPACE/build_utils/docker/ci_stack/project_images/build_in_jenkins_image/dockerfile   --build-arg archive=$RELEASE_ARTEFACT_NAME -t $2 .
+sudo docker build --build-arg archive=$RELEASE_ARTEFACT_NAME -t $2 .
 
 
 
