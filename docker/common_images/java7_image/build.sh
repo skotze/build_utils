aaa213def794c2afa2bc41f1dc3a8bea1c7ae47d
Stephan Kotze <Stephanus.Kotze@gmail.com>
2016-09-13 12:38:01 +0100

    added java 7 image

---
+++ docker/common_images/java7_image/build.sh
@@ -0,0 +1,2 @@
+docker build --no-cache -t java7_base .
+docker tag java7_base java7_base:latest
---
+++ docker/common_images/java7_image/tag_and_push.sh
@@ -0,0 +1,2 @@
+docker tag java8_base:latest osboxes:5000/java8_base:latest
+docker push osboxes:5000/java8_base:latest
